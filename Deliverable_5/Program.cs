6a7053c510b31473f413fcaffb1eb95fce24405c
Yihui <Yihui@DESKTOP-LD7FC88>
2023-02-16 16:35:03 -0500

    Add project files.

---
+++ Deliverable_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Methods_Application
+{
+    class Program
+    {
+        static int[] random_array(int array_len)
+        {
+            Random random = new Random();
+            int[] MyArray = new int[array_len];
+            for (int i = 0; i < array_len; i++)
+            {
+                MyArray[i] = random.Next(10, 50);
+            }
+            return MyArray;
+        }
+
+        static int sum(int[] array)
+        {
+            int sum = 0;
+            foreach(int item in array)
+            {
+                sum = sum + item;
+            }
+            return sum;
+        }
+
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Enter an integer between 5 and 15");
+            int input = int.Parse(Console.ReadLine());
+
+            int[] sampleArray = random_array(input);
+
+            Console.WriteLine("The elements in the random array are: ");
+            foreach (int element in sampleArray)
+            {
+                Console.WriteLine(element + " ");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("The sum is: " + sum(sampleArray));
+        }
+       
+    }
+}
